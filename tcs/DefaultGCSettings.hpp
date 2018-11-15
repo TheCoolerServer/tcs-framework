e393b295e81dbe7f5f8e8c7cdcd3b11273b8daa1
gkovalechyn <gkovalechyn@gmail.com>
2018-11-15 03:01:07 -0200

    Added default GC settings

---
+++ tcs/DefaultGCSettings.hpp
@@ -0,0 +1,12 @@
+//https://community.bistudio.com/wiki/Description.ext#Corpse_.26_wreck_management
+corpseManagerMode = 1;
+corpseLimit = 15;
+corpseRemovalMinTime = 10;
+corpseRemovalMaxTime = 600; //10 minutes
+
+wreckManagerMode = 1;
+wreckLimit = 10;
+wreckRemovalMinTime = 10;
+wreckRemovalMaxTime = 1200; //20 minutes
+
+minPlayerDistance = 25; //25 meter radius
