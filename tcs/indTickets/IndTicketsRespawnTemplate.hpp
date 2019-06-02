0ef28a117cc57e7af3bc0ed11e8e31bdae14c924
Calvin Melanson <kirbsys@gmail.com>
2019-06-02 06:00:10 +0000

    Replace IndTicketsRespawnTemplate.hpp

--- tcs/indTickets/IndTicketsRespawnTemplate.hpp
+++ tcs/indTickets/IndTicketsRespawnTemplate.hpp
@@ -1,4 +1,4 @@
 class TCS_Ind_Tickets {
-  onPlayerKilled = "tcs\indTickets\fn_ticketsOnPlayerKilled.sqf";
-  onPlayerRespawn = "tcs\indTickets\fn_ticketsOnPlayerRespawn.sqf";
+  onPlayerKilled = "tcs\indTickets\fn_indTicketsOnPlayerKilled.sqf";
+  onPlayerRespawn = "tcs\indTickets\fn_indTicketsOnPlayerRespawn.sqf";
 };
