835cd557871dbe4660df0ad7e0c06d70d51de3bf
Jimmie  Rustler <thetrueballer95@gmail.com>
2021-12-12 17:28:16 +0000

    Update tcs/headless_ai/headless_ai/cfgXEH/init/ServerPreInit.sqf, tcs/headless_ai/headless_ai/cfgXEH/init/ClientPreInit.sqf, tcs/headless_ai/headless_ai/cfgXEH/init/PreInit.hpp

---
+++ tcs/headless_ai/headless_ai/cfgXEH/init/PreInit.hpp
@@ -0,0 +1,5 @@
+class PZAI_PreInit {
+    Init = "call compile preProcessFileLineNumbers 'tcs\headless_ai\headless_ai\cfgXEH\init\GlobalPreInit.sqf';";
+    serverInit = "call compile preProcessFileLineNumbers 'tcs\headless_ai\headless_ai\cfgXEH\init\ServerPreInit.sqf';";
+    clientInit = "call compile preProcessFileLineNumbers 'tcs\headless_ai\headless_ai\cfgXEH\init\ClientPreInit.sqf';";
+};
