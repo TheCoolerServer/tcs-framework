54ee8a9890160d4de6406387fccab22dcc401636
Gabriel Kovalechyn <gkovalechyn@gmail.com>
2022-10-11 09:59:00 -0300

    Added validation for framework version

--- .github/scripts/master-pr-validations.ps1
+++ .github/scripts/master-pr-validations.ps1
@@ -1,5 +1,19 @@
 Param($SourceBranch)
+Param([string[]] $ChangedFiles)
 
 if (!$SourceBranch -eq "dev" ) { 
 	Throw "Pull requests to the master branch must only be made from the dev branch";
+}
+
+$HasChangedFrameworkVersion = $false
+
+foreach ($file in $ChangedFiles) {
+	if ($file -match "FrameworkVersion\.hpp$") {
+		$HasChangedFrameworkVersion = $true;
+	}
+}
+
+
+if (!$HasChangedFrameworkVersion) {
+	Throw "PRs to the master branch must change the framework version because a new release will be created";
 }
