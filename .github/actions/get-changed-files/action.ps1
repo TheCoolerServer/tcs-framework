f0f9d74a086de21fab396b59c77c0445d81f54f7
Gabriel Kovalechyn <gkovalechyn@gmail.com>
2023-01-31 22:24:04 -0300

    Log API response

--- .github/actions/get-changed-files/action.ps1
+++ .github/actions/get-changed-files/action.ps1
@@ -2,15 +2,17 @@ Param(
 	[string] $Pr
 )
 
-$ApiResponse = ConvertFrom-Json (
-	gh api `
-		-H "Accept: application/vnd.github+json" `
-		"/repos/TheCoolerServer/tcs-framework/pulls/$Pr/files"
-)
+$RawAPIReponse = gh api `
+-H "Accept: application/vnd.github+json" `
+"/repos/TheCoolerServer/tcs-framework/pulls/$Pr/files"
+
+Write-Output "Received API response: $RawAPIReponse"
+
+$ApiResponse = ConvertFrom-Json $RawAPIReponse
 
 $FileNames = $ApiResponse.ForEach({ $_.filename })
 
-Write-Output ("Foud changed files:" -f ($FileNames -join "`n"))
+Write-Output ("Found changed files:" -f ($FileNames -join "`n"))
 
 $CommaSeparatedFiles = $FileNames -join ","
 
