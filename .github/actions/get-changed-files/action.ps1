62694ad4bb72038aa6fbfce812fa2329839d72d6
Gabriel Kovalechyn <gkovalechyn@gmail.com>
2023-01-31 22:37:55 -0300

    Fixed not writing to output file

--- .github/actions/get-changed-files/action.ps1
+++ .github/actions/get-changed-files/action.ps1
@@ -7,16 +7,12 @@ $RawAPIReponse = gh api `
 	"/repos/TheCoolerServer/tcs-framework/pulls/$Pr/files?per_page=100"
 $ApiResponse = ConvertFrom-Json $RawAPIReponse
 
-Write-Output "API response: $ApiResponse"
-
 $FileNames = $ApiResponse.ForEach({ $_.filename })
 
-Write-Output "File names: $FileNames"
-
-Write-Output ("Found changed files: {0}" -f ($FileNames -join "`n"))
+Write-Output ("Found changed files: `n{0}" -f ($FileNames -join "`n"))
 
 $CommaSeparatedFiles = $FileNames -join ","
 
 Write-Output "Writing output: $CommaSeparatedFiles"
 
-"changed-files=$CommaSeparatedFiles" >> $GITHUB_OUTPUT
+"changed-files=$CommaSeparatedFiles" >> $Env:GITHUB_OUTPUT
