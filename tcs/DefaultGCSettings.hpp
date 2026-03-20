9e67781e2af7806f726689695f7807636cadc689
SoylentScience <128281850+SoylentScience@users.noreply.github.com>
2026-03-19 18:15:44 -0700

    Update DefaultGCSettings.hpp
    Increased corpse counts and minimum de-spawn times

--- tcs/DefaultGCSettings.hpp
+++ tcs/DefaultGCSettings.hpp
@@ -1,12 +1,12 @@
 //https://community.bistudio.com/wiki/Description.ext#Corpse_.26_wreck_management
 corpseManagerMode = 1;
-corpseLimit = 15;
-corpseRemovalMinTime = 10;
+corpseLimit = 40;
+corpseRemovalMinTime = 300; //5 minutes
 corpseRemovalMaxTime = 600; //10 minutes
 
 wreckManagerMode = 1;
-wreckLimit = 10;
-wreckRemovalMinTime = 10;
+wreckLimit = 15;
+wreckRemovalMinTime = 300; //5 minutes
 wreckRemovalMaxTime = 1200; //20 minutes
 
 minPlayerDistance = 25; //25 meter radius
